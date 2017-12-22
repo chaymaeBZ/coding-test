1688c07b0fce94773c0068c5b8f9f2b278d03a3a
chaymaeBZ <bazzaoui.chaymae@gmail.com>
2017-12-22 16:18:23 +0000

    test tweets controller

--- Gemfile
+++ Gemfile
@@ -46,6 +46,14 @@ group :development, :test do
   # Adds support for Capybara system testing and selenium driver
   gem 'capybara', '~> 2.13'
   gem 'selenium-webdriver'
+
+  # use rspec
+  gem "rspec-rails" 
+  gem 'rails-controller-testing'
+  gem 'factory_bot'
+
+  # awesome print
+  gem 'awesome_print'
 end
 
 group :development do
---
+++ spec/controllers/tweets_controller_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe TweetsController, :type => :controller do
+  let(:mentions) { Mention.all }
+
+  context "GET index" do
+     before(:each) { get :index, {format: :js, xhr: true} }
+    
+    it "get all mentions stores them" do
+      
+      expect(assigns[:mentions]).to match_array mentions
+    end
+
+    it "renders the index template" do
+      
+      expect(response).to render_template("index")
+    end
+
+  end 
+
+
+
+end
