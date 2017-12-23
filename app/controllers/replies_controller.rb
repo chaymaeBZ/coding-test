d6a37e72c9cd8596daad2e34ef9f32627be5af90
chaymaeBZ <bazzaoui.chaymae@gmail.com>
2017-12-23 03:08:32 +0000

    reply counter front-end validation + scenario 1.0 isolated

--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -7,9 +7,19 @@ class RepliesController < ApplicationController
   def create
     tweet = Mention.find_by!(tweet_id: params[:tweet_id])
     _response = @client.update(
-      "@#{tweet.user_screen_name} #{params[:reply][:body]}",
-      in_reply_to_status_id: params[:tweet_id]
-    )
+               "#{params[:reply][:body]}",
+                 in_reply_to_status_id: params[:tweet_id] 
+                )
+
+    # Scenario 1.0 :
+    #
+    # _response = @client.update(
+    #             "@#{tweet.user_screen_name} #{params[:reply][:body]}",
+    #              in_reply_to_status_id: params[:tweet_id]
+    #              )
+    #
+    # For more complexe use case
+    # where client wants the reply tweet to apear in replies section ...
 
     flash[:positive] = 'Your reply has been sent'
   rescue Twitter::Error::Forbidden, ActiveRecord::RecordNotFound
